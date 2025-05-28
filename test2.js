f0eda6763bb5a3afc99159e4821c6480bd0d940a
tel3port <carn3lian@gmail.com>
2025-05-28 19:53:27 -0300

    still learning about commits 2

--- test2.js
+++ test2.js
@@ -3,3 +3,4 @@ console.log("added more code here")
 console.log("added more code here 2")
 console.log("added more code here 3")
 console.log("added more code here 4");
+console.log("added more code here 5");
